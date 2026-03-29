74cadc01194e9fdcb16e74ee8f9b775ca694f1c1
Background <76162743+YaIiya@users.noreply.github.com>
2026-03-30 03:52:31 +0700

    shart

--- scripts/sc55_rom_decomp_decoders.py
+++ scripts/sc55_rom_decomp_decoders.py
@@ -1,6 +1,5 @@
-# import numpy as np
-
 from sc55_rom_decomp_globals import *
+
 char = np.uint8
 uint8_t = np.uint8
 int8_t = np.int8
@@ -11,8 +10,29 @@ long = np.int32
 ulonglong = np.uint64
 longlong = np.int64
 
+def rpcm_decode(data: np.int8 | np.uint8 = None) -> int:
+    # from mamedev/mame/src/devices/sound/rolandpcm.cpp
+    # RPCM is just my name for this specific pcm type.
+    if not data: return 0
+    if not isinstance(data, (np.int8, np.uint8)): raise TypeError("Input must be of type numpy.int8 or numpy.uint8!")
+    data = int(data.view(np.int8)) if isinstance(data, np.uint8) else int(data)
+    if data < 0:
+        sign = -1
+        val = -data
+    else:
+        sign = 1
+        val = data
+    shift = val >> 4
+    val &= 15
+    if not shift:
+        return val * sign
+    else:
+        return ((0x10 + val) << (shift - 1)) * sign
+    # CM32L pcm
+
 def ldpcm_decode(data: char = 0):
     """
+    LDPCM is a DPCM compression type used on all of Roland's GS synths, and was named as such by Edward d-tech
     this is a dummy function for now
     """
     pass
